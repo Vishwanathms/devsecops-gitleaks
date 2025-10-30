e5b7e06e770267e5de7ea948cd2bcf3670bda5bb
Vishwanathms <vishwanath.murthy@gmail.com>
2025-10-30 09:49:11 +0530

    changed the python app to container

--- python_app/app.py
+++ python_app/app.py
@@ -24,5 +24,10 @@ def get_key():
     api_key = os.getenv("API_KEY", "No API key found")
     return jsonify({"api_key": api_key})
 
+@app.route('/')
+def home():
+    return jsonify({"message": "Flask app running. Use /apikey to get the API key."})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
